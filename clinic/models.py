11bd485815b821419ab5b1b4f7c6d2897a25e35a
RalitsaLuka <rali4ka1900@abv.bg>
2020-06-02 23:36:41 +0300

    Added form for add animal

--- clinic/models.py
+++ clinic/models.py
@@ -63,6 +63,7 @@ class Animal(models.Model):
     species = models.CharField(max_length=250)
     description = models.TextField()
 
+
     def __str__(self):
         return f'Animal "{self.name}"'
 
--- clinic/urls.py
+++ clinic/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path, include
-from clinic.viewss import index
+from clinic.viewss import index, animals
 
 
 app_name = 'clinic'
 
 
+animals_patterns = [
+    path('', animals.list, name='list'),
+    path('<int:animal_id>/', animals.detail, name='detail'),
+    path('new/', animals.AnimalCreateView.as_view(), name='create'),
+]
+
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('animal/', animals.AnimalCreateView.as_view(), name="animal"),
 ]
